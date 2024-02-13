38651fd515c4d32f170543ac0f7ca15bdb8fcbdb
Andrew Domingues <drewdomi@proton.me>
2024-02-13 12:12:34 -0300

    feat: add waybar & sway config

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,16 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-
 end
+set -gx VOLTA_HOME "$HOME/.volta"
+set -gx PATH "$VOLTA_HOME/bin" $PATH
+set -gx SDKMAN_DIR "$HOME/.sdkman"
 
-set -x TERM tmux-256color
+# pnpm
+set -gx PNPM_HOME "/home/drew/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+    set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
 
+abbr --add cl clear
+set -U fish_greeting
