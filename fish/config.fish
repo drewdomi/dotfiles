769e0303b1bc9e6980486f085a3b40a64e19ae28
Andrew Domingues <drewdomi@proton.me>
2024-02-29 00:35:10 -0300

    feat: dunst, wlogout, swaylock, etc

--- fish/config.fish
+++ fish/config.fish
@@ -1,16 +1,23 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
-set -gx VOLTA_HOME "$HOME/.volta"
+
+set -U fish_greeting
+
 set -gx PATH "$VOLTA_HOME/bin" $PATH
+
+# Set Volta PATH
+set -gx VOLTA_HOME "$HOME/.volta"
+
+# Set Volta SDKMAN PATH
 set -gx SDKMAN_DIR "$HOME/.sdkman"
 
-# pnpm
+# Set PNPM PATH
 set -gx PNPM_HOME "/home/drew/.local/share/pnpm"
 if not string match -q -- $PNPM_HOME $PATH
     set -gx PATH "$PNPM_HOME" $PATH
 end
-# pnpm end
 
+# Fix pipewire not work properly on enter in Hyprland
+abbr --add fix-pipewire "pkill pipewire; pipewire & disown"
 abbr --add cl clear
-set -U fish_greeting
