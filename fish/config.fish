bb1d42b77a8fd71713410ecb0e03fdeedfb98a48
Andrew Domingues <drewdomi@proton.me>
2024-03-09 18:38:22 -0300

    feat: config update
    - alacritty
    - kitty
    - waybar
    - wofi
    - wlogout
    - hyprland
    - fish

--- fish/config.fish
+++ fish/config.fish
@@ -2,22 +2,11 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-set -U fish_greeting
-
-set -gx PATH "$VOLTA_HOME/bin" $PATH
-
-# Set Volta PATH
-set -gx VOLTA_HOME "$HOME/.volta"
-
-# Set Volta SDKMAN PATH
-set -gx SDKMAN_DIR "$HOME/.sdkman"
-
-# Set PNPM PATH
+# pnpm
 set -gx PNPM_HOME "/home/drew/.local/share/pnpm"
 if not string match -q -- $PNPM_HOME $PATH
     set -gx PATH "$PNPM_HOME" $PATH
 end
 
-# Fix pipewire not work properly on enter in Hyprland
-abbr --add fix-pipewire "pkill pipewire; pipewire & disown"
+set -U fish_greeting ""
 abbr --add cl clear
