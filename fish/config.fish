84d506e224c702354cc6d30a5327754b0dfa855e
Andrew Domingues <drewdomi@proton.me>
2024-05-16 18:24:57 -0300

    updated: tmux, fish

--- fish/config.fish
+++ fish/config.fish
@@ -9,4 +9,10 @@ if not string match -q -- $PNPM_HOME $PATH
 end
 
 set -U fish_greeting ""
+set -gx VOLTA_HOME "$HOME/.volta"
+set -gx PATH "$VOLTA_HOME/bin" $PATH
+
 abbr --add cl clear
+abbr --add vi nvim
+
+abbr --add runMain "cd src/; javac -d ../bin Main.java && cd ../bin; java Main; cd ../"
